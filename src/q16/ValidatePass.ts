ed0f304ac4665b153eb65857f58a04002ffad5af
Applelyyyy <islandgameming@gmail.com>
2025-12-15 17:50:53 +0700

    Final10

--- src/q16/ValidatePass.ts
+++ src/q16/ValidatePass.ts
@@ -2,15 +2,17 @@ const User_lc9 = process.argv[2];
 if (User_lc9 === undefined) process.exit();
 let r = false;
 let s = false;
+let t = false;
 if (User_lc9.length >= 8) {
     for (let p = 0; p <= User_lc9.length; p++){
         if(User_lc9[p] === undefined){
             break
         }
         if (isNaN(Number(User_lc9[p])) && (User_lc9[p].toUpperCase() === User_lc9[p])){
-            if (User_lc9[p].toLowerCase() === User_lc9[p]){
-                r = true
-            }
+            r = true
+        }
+        if (User_lc9[p].toLowerCase() === User_lc9[p]){
+            t = true
         }
         else{
             s = true
